38b08bf944bdf63e090ad635341ebe44df909618
Jason Turpin <jason_turpin@yahoo.com>
2021-05-02 20:55:49 -0500

    Update

--- azure_script/setup_vm2.sh
+++ azure_script/setup_vm2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get -y update       ssh -t
-sudo apt-get -y upgrade  ssh -t
-sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth ssh -t
+sudo apt-get -y update       
+sudo apt-get -y upgrade  
+sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
 git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
 
 cd azure-cloud-mining-script
